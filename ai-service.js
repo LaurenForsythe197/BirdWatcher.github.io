80b7560280f8f68446a326e3f4eb63d72e713433
LaurenForsythe197 <laurenforsythe197@gmail.com>
2026-07-05 20:15:26 +0100

    Update ai-service.js
    Change to use Cloudflare worker

--- ai-service.js
+++ ai-service.js
@@ -3,41 +3,12 @@ window.AI = {
     identifyBird: async function(imageData) {
 
         const response = await fetch(
-            fetch("https://birdwatcher.yourname.workers.dev/identifyBird",
+            fetch("https://birdwatcher-ai.laurenforsythe197.workers.dev/",
             {
                 method: "POST",
                 headers: { "Content-Type": "application/json"},
                 body: JSON.stringify({
-                    model: "gpt-4.1",
-                    input: [
-                        {
-                            role: "user",
-                            content: [
-                                {
-                                    type: "input_text",
-                                    text:
-                                    `Identify this bird.
-                                    Return ONLY valid JSON.
-                                    
-                                    {
-                                      "species_name":"",
-                                      "common_name":"",
-                                      "scientific_name":"",
-                                      "confidence":"high|medium|low",
-                                      "estimated_age":"adult|juvenile|unknown",
-                                      "gender":"male|female|unknown",
-                                      "identifying_features":"",
-                                      "habitat":"",
-                                      "fun_fact":""
-                                    }`
-                                },
-                                {
-                                    type: "input_image",
-                                    image_url: imageData
-                                }
-                            ]
-                        }
-                    ]
+                    image: imageData
                 })
             }
         );
@@ -46,10 +17,6 @@ window.AI = {
             throw new Error("OpenAI returned " + response.status);
         }
 
-        const data = await response.json();
-        console.log(data);
-        const text =
-            data.output[0].content[0].text;
-        return JSON.parse(text);
+        return await response.json();
     }
 };
